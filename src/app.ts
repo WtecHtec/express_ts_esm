9ea95cbd6a26c66c4c88497d004488cd6d5b791f
sr7 <504105925@qq.com>
2024-04-07 09:56:19 +0800

    feat:static

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { GetPath } from "./util/common";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(express.static(GetPath("/public")));
 
 const PORT = process.env.PORT || 5698;
 app.listen(PORT, () => {
